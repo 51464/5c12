6719be403cdec17dc1eaba11d567e74242e306e7
51464 <73089610+51464@users.noreply.github.com>
2020-11-02 11:06:00 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,15 @@ input.onButtonPressed(Button.A, function () {
     music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Once)
     chance = 3
     basic.showNumber(chance)
-    while (chance > 0) {
-    	
+    while (chance > 1) {
+        if (input.pinIsPressed(TouchPin.P1)) {
+            music.playTone(262, music.beat(BeatFraction.Sixteenth))
+            chance += -1
+            basic.showNumber(chance)
+        }
     }
+    music.startMelody(music.builtInMelody(Melodies.JumpDown), MelodyOptions.Once)
+    basic.showIcon(IconNames.Sad)
 })
 let chance = 0
 basic.showIcon(IconNames.EigthNote)
